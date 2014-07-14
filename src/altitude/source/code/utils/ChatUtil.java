dd0ce8fdd2b0fbb402c3329aedb57092648c9f40
Mali Plavi <marko.raspica@gmail.com>
2014-07-14 11:48:20 +0200

    Change ChatUtil

--- src/altitude/source/code/utils/ChatUtil.java
+++ src/altitude/source/code/utils/ChatUtil.java
@@ -1,5 +1,19 @@
 package altitude.source.code.utils;
 
-public class ChatUtil {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
+public class ChatUtil {
+	public void Brodcast(String msg){
+		for(Player p : Bukkit.getOnlinePlayers()){
+			p.sendMessage(Brodcaster() + msg);
+		}
+		
+	}
+	
+	private String Brodcaster() {
+		return ChatColor.DARK_AQUA + "[Mario Party Battle] " + ChatColor.GOLD;
+	}
+	
 }
